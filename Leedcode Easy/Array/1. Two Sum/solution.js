7356cb90a7763be1c074cd71c4a5abdbf39ddce8
Vinskao <tianyikao@gmail.com>
2023-09-18 22:32:29 +0800

    0.004

---
+++ Leedcode Easy/Array/1. Two Sum/solution.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// var twoSum = function(nums, target) {
+//     let anws = [];
+
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             const num1 = nums[i];
+//             const num2 = nums[j];
+//             if (num1 + num2 === target) {
+//                 anws.push([i, j]);
+//             }
+//         }
+//     }
+
+//     return anws;
+// };
+
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+    return [];
+};
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+const result = twoSum(nums, target);
+console.log(result);
