29ecad04a168725970c3c5509045f57bf6b03478
Vinskao <tianyikao@gmail.com>
2023-09-19 21:32:27 +0800

    0.005

--- Leedcode Easy/Tree/94. Binary Tree Inorder Traversal/solution2.js
+++ Leedcode Easy/Tree/94. Binary Tree Inorder Traversal/solution2.js
@@ -12,15 +12,19 @@
  */
 var inorderTraversal = function(root) {
     let ans = [];
+    let stack = [];
+    let currentNode = root;
 
-    function traverse(node) {
-        if (node !== null) {
-            traverse(node.left);
-            ans.push(node.val); // 紀錄最左邊節點的值
-            traverse(node.right);
+    while (currentNode || stack.length > 0) { //直到 currentNode 变为 null或stack還有Node
+        while (currentNode){  //直到 currentNode 变为 null，也就是当前节点没有左子节点为止。
+            stack.push(currentNode);
+            currentNode = currentNode.left //如果 currentNode 没有左子节点则 currentNode 将变为 null
         }
-    }
 
-    traverse(root);
-    return ans;
+        currentNode = stack.pop(); //从stack中弹出最後一个节点-当前子树的根节点入currentNode
+
+        ans.push(currentNode); //将弹出的节点的值添加到ans
+
+        currentNode = currentNode.right; //将 currentNode 更新为当前节点的右子节点
+    } 
 }
