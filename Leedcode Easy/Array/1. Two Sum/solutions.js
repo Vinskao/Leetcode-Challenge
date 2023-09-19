456e2ed76661dc63013372455cd5894b73b04f8e
Vinskao <tianyikao@gmail.com>
2023-09-19 09:48:31 +0800

    0.0042

--- Leedcode Easy/Array/1. Two Sum/solutions.js
+++ Leedcode Easy/Array/1. Two Sum/solutions.js
@@ -1,11 +1,17 @@
 var twoSum = function(nums, target) {
-    let indices = {};
+    const numIndices = {};
 
     for (let i = 0; i < nums.length; i++) {
         const complement = target - nums[i];
-        if (indices[complement] !== undefined) {
-            return [indices[complement], i];
-
+        if (numIndices[complement] !== undefined) {
+            return [numIndices[complement], i]
+        } 
+        
+        numIndices[nums[i]] = i; // 將當前數字的索引儲存到 numIndices
+    }
+    return [];
+}
+    
 
 
 
@@ -13,3 +19,13 @@ const nums = [2, 7, 11, 15];
 const target = 9;
 const result = twoSum(nums, target);
 console.log(result);
+
+const nums1 = [3, 2, 4];
+const target1 = 6;
+const result1 = twoSum(nums1, target1);
+console.log(result1);
+
+const nums2 = [3, 3];
+const target2 = 6;
+const result2 = twoSum(nums2, target2);
+console.log(result2);
