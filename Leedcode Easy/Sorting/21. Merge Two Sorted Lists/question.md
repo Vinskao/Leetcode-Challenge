ef5e6ec74b98772d4e6f87a4d006bb26226b4dd8
Vinskao <tianyikao@gmail.com>
2023-09-20 17:16:54 +0800

    another sorting

--- Leedcode Easy/Sorting/21. Merge Two Sorted Lists/question.md
+++ Leedcode Easy/Sorting/21. Merge Two Sorted Lists/question.md
@@ -27,3 +27,14 @@ Constraints:
 The number of nodes in both lists is in the range [0, 50].
 -100 <= Node.val <= 100
 Both list1 and list2 are sorted in non-decreasing order.
+
+
+# Intuition
+用node建構一個新的linkList原點，或arr[0]，透過next指向下一個index並使用while及if反覆確認List1、List2大小後，由小到大填入值進新的linkList。
+# Approach
+定義時List1、List2即可代表List1[0]、List2[0]，順著index比較大小並填入linkList，最後將剩餘的List元素(較大的)放入，在if函數中沒被放入的list元素會繼續在下一次被比較。
+# Complexity
+- Time complexity:
+O(m + n)，因为在 while 循环中，我们遍历了两个链表一次，每次比较两个节点的值，然后将较小的节点添加到合并后的链表中。
+- Space complexity:
+O(1)，没有使用额外的数据结构来存储节点。
--- Leedcode Easy/Sorting/21. Merge Two Sorted Lists/solution.js
+++ Leedcode Easy/Sorting/21. Merge Two Sorted Lists/solution.js
@@ -10,6 +10,23 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    
-};
+var mergeTwoLists = function(list1, list2) { // list1 和 list2 分别代表两个已排序链表的first node = arr[0]
+    const tempNode = new ListNode(0); // 创建一个新节点tempNode
+    tempNode.next = null; // 初始化节点来构建链表
+
+    let currentNode = tempNode; // tempNode去currentNode取值
+
+    while (list1 !== null && list2 !== null) {  // 在list接不為null下持續循環，順序是根据节点的值大小
+        if (list1.val < list2.val) { // 比较两个链表节点的值，找出list1节点值较list2小的節點
+            currentNode.next = list1; // 放該list1節點入currentNode的下個節點
+            list1 = list1.next; // 移动list1(currentNode的下個節點)指针到下一个节点(currentNode的下下個節點)
+        } else { // 該list1节点值较list2节点值大
+            currentNode.next = list2; // 放該list2節點入currentNode的下個節點
+            list2 = list2.next; // 移动list2 (currentNode的下個節點) 指针到下一个节点
+        }
+        currentNode = currentNode.next; // 移动 currentNode 指针到下一个节点
+    }
+
+    currentNode.next = list1 || list2; // 其中一個list已經為null，剩下的值放入currentNode的下個節點
+    return tempNode.next; // next 指向合并后的链表的第一个元素
+}
