cd4f1a238f180a2c607dd99c12ec22a0b5872224
Vinskao <tianyikao@gmail.com>
2023-11-25 18:30:02 +0800

    26

--- Leedcode Easy/Array/26. Remove Duplicates from Sorted Array/Solution.java
+++ Leedcode Easy/Array/26. Remove Duplicates from Sorted Array/Solution.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 class Solution {
     public int removeDuplicates(int[] nums) {
-        return nums.length;
+
+        if(nums.length == 0){
+            return 0;
+        }
+        int j =1 ;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] != nums[i-1]){
+                nums[j] = nums[i];
+                j++;
+            }
+
+        }
+        System.out.println(j);
+
+        return j;
+
+    }
+
+    public static void main(String[] args){
+
+        Solution solution = new Solution();
+        int[] nums = {1,5,6,10,10};
+        int result = solution.removeDuplicates(nums);
+        System.out.println(result);
+
     }
 }
+
+
+
+
