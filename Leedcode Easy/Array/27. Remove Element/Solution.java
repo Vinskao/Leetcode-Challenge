49e3a94e43c719a885978adddf2405c427d16eab
Vinskao <tianyikao@gmail.com>
2023-11-27 18:29:54 +0800

    init

--- Leedcode Easy/Array/27. Remove Element/Solution.java
+++ Leedcode Easy/Array/27. Remove Element/Solution.java
@@ -1,7 +1,16 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        
+        for(int i=1; i<= nums.length; i++){
+            
+            if(nums[i] == val ){
+                nums[i] = 0;
+            } else {
+                
+            }
+            
 
+
+        }        
         return 0;
     }
 }
