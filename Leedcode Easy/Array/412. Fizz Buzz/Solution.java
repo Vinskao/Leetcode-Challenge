07308cf9397fe0778a72ba5892a1ddc810babca0
Vinskao <tianyikao@gmail.com>
2023-11-28 17:19:16 +0800

    412

---
+++ Leedcode Easy/Array/412. Fizz Buzz/Solution.java
@@ -0,0 +1,27 @@
+import java.util.List;
+
+class Solution {
+    public List<String> fizzBuzz(int n) {
+
+
+        List<String> l= null;
+
+        int size = l.size();
+
+        for (int i=0; i<size; i++) {
+            if (i%3==1){
+
+            } else if (i%5==1) {
+
+            } else if (i%15==1){
+
+            } else {
+                
+            }
+        }
+
+
+
+        return null;
+    }
+}
