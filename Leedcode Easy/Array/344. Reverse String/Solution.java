9f7b4c679186e4e5ffa496c02a3dd0be0a3a0f85
Vinskao <tianyikao@gmail.com>
2023-11-30 22:09:39 +0800

    344

--- Leedcode Easy/Array/344. Reverse String/Solution.java
+++ Leedcode Easy/Array/344. Reverse String/Solution.java
@@ -1,5 +1,13 @@
 class Solution {
     public void reverseString(char[] s) {
-        
+        int firstIndex = 0;
+        int finalIndex = s.length - 1;
+      
+        for (int i = firstIndex; i < finalIndex; i++){
+            char temp = s[i];
+            s[i] = s[finalIndex];
+            s[finalIndex] = temp;
+            finalIndex--;
+        }
     }
 }
