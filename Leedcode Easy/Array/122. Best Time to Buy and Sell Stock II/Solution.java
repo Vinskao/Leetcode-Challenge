e6b2a3c04f0f18e5982e031b325969f1f86941e7
Tianyi Kao <tianyikao@gmail.com>
2024-09-03 19:53:50 +0800

    Update Solution.java

--- Leedcode Easy/Array/122. Best Time to Buy and Sell Stock II/Solution.java
+++ Leedcode Easy/Array/122. Best Time to Buy and Sell Stock II/Solution.java
@@ -4,12 +4,14 @@ class Solution {
     }
 
     public int maxProfit(int[] prices) {
+        int profit = 0;
         for(int i = 0; i < prices.length; i++){
-            if (i > 0)
-            prices[i] 
+            if (i > 0 && prices[i] > prices[i-1]){
+                profit = prices[i] - prices[i-1];                
+            } 
         }
 
-        return 0;
+        return profit;
 
     }
 }
