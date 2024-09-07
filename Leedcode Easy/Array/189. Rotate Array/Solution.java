6590903e395db5b6fb6547d94da0d261da05cd49
Vinskao <tianyikao@gmail.com>
2024-09-08 00:33:15 +0800

    189

--- Leedcode Easy/Array/189. Rotate Array/Solution.java
+++ Leedcode Easy/Array/189. Rotate Array/Solution.java
@@ -3,16 +3,21 @@ class Solution {
         k = k % nums.length;
         int[] shifter = new int[nums.length - k];
         int[] stayer = new int[k];
+
         for (int i = 0; i < nums.length; i++) {
             if (i < nums.length - k) {
                 shifter[i] = nums[i];
-                nums[i] = shifter[i];
             } else {
                 stayer[i - (nums.length - k)] = nums[i];
-                nums[i] = stayer[i];
             }
         }
-        System.arraycopy(stayer, 0, nums, 0, stayer.length);
-        System.arraycopy(shifter, 0, nums, stayer.length, shifter.length);
+
+        for (int i = 0; i < k; i++) {
+            nums[i] = stayer[i];
+        }
+
+        for (int i = 0; i < nums.length - k; i++) {
+            nums[i + k] = shifter[i];
+        }
     }
 }
