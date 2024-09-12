e932bf0f0c926f000b1698ca0dd1472bcb9b5e38
Vinskao <tianyikao@gmail.com>
2024-09-12 19:19:36 +0800

    217

---
+++ Leedcode Easy/Array/217. Contains Duplicate/Solution2.java
@@ -0,0 +1,14 @@
+public class Solution2 {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
